afff0b60cba7cd9d07a82dd9af03c8943e7d397f
Brad Koch <bradkoch2007@gmail.com>
2014-06-17 17:42:00 -0400

    Explicitly declared segment v0.4.2
    LATEST is a bad idea.  A breaking change was introduced in version
    1.0.0, causing this library to stop compiling.
    
    Setting to 0.4.2, last known good version.  It can be updated later if
    the Clojure code is updated to match.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :plugins [[codox "0.6.6"]]
   :codox {:src-linenum-anchor-prefix "L"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.github.segmentio/analytics "LATEST"]]
+                 [com.github.segmentio/analytics "0.4.2"]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.13"]
                                   [ch.qos.logback/logback-core "1.0.13"]
                                   [org.slf4j/jcl-over-slf4j "1.7.5"]]}})
